3efa99c4876e5ffb5131301f00014707c4001af8
Gabriel Fantin <99855155+GabrielFantin-dev@users.noreply.github.com>
2026-09-25 17:33:46 -0300

    Atualizar o Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,4 +1,25 @@
 class Cadastro{
 
+   private id;
+   private senha;
 
+public void setid() {
+        this.id;
+    }
+
+
+public String getid() {
+        return id;
+    }
+
+
+public void setsenha() {
+        this.id;
+    }
+
+
+public String getsenha() {
+
+        return senha;
+    }
 }
